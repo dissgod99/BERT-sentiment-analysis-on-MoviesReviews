fb0f57ab57855dec4075469b1e1acd9c56fb3cb8
Aziz Chikh <azizchpay99@outlook.com>
2023-03-10 16:01:21 +0100

    Clean preprocess file

--- Code/preprocess_dataset.py
+++ Code/preprocess_dataset.py
@@ -9,8 +9,6 @@ TRAIN_DIR = "Dataset/Train.csv"
 DEV_DIR = "Dataset/Valid.csv"
 TEST_DIR = "Dataset/Test.csv"
 
-
-
 def remove_line_breaks(text: str) -> str:
     out = re.sub(r'<br /><br />', '', text)
     return out
@@ -32,13 +30,9 @@ def edit_directory_csv_file(directory:str, added_str:str) -> str:
 def load_df_as_pandas(directory:str, preprocess=False, export=False) -> pd.DataFrame:
     df = pd.read_csv(directory, sep=",")
     if preprocess:
-        #dict_out = dict()
+
         sentiment = df["label"].apply(lambda x: "Positive" if x == 1 else "Negative")
-        #preprocessed_text = df["text"].apply(lambda x: remove_spoiler_alerts(remove_line_breaks(x)))
         preprocessed_text = df["text"].apply(lambda x: remove_line_breaks(x))
-        #dict_out["text"] = preprocessed_text
-        #dict_out["label"] = df["label"]
-        #dict["sentiment"] = sentiment
 
         dict_out = {
             "text": preprocessed_text,
@@ -51,20 +45,6 @@ def load_df_as_pandas(directory:str, preprocess=False, export=False) -> pd.DataF
             df_preprocessed.to_csv(new_dir, index=False, header=True)
         return df_preprocessed
     return df
-
-
-
-
-if __name__=="__main__":
-    df = pd.read_csv(TRAIN_DIR)
-    print(df["text"].iloc[104])
-    print("++++++++++++++++++++++++++++++")
-    #print(remove_spoiler_alerts(remove_line_breaks(df["text"].iloc[104])))
-
-    """df_1 = load_df_as_pandas(DEV_DIR, preprocess=True, export=True)
-    print(df_1.head(110))"""
-    df_2 = load_df_as_pandas(TRAIN_DIR, preprocess=True, export=True)
-    print(df_2.head(110))
 
     
     
